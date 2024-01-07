f016c2f30dfb0ee9ca2b57c727c08cc604dd71b5
kaigo <r202203404ww@jindai.jp>
2024-01-07 16:00:16 +0900

    for a b c

--- port.c
+++ port.c
@@ -3,12 +3,14 @@
 int main(void){
     int a=50;
     int b=10;
+    int c=80;
     printf("hello world.\n");
     for(int i=0;i<10;i++){
         a=a+i;
         b=b+i;
+        c=c+a-b;
         printf("a=%d.\n",a);
-        printf("a=%d.b=%d.\n",a,b);
+        printf("a=%d.b=%d.c=%d.\n",a,b,c);
     }
     return 0;
 }
